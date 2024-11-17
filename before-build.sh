2fe62ae805dbe268d7b1f6717c9f268a59f7d413
Santosh Bandichode <santoshg550@gmail.com>
2024-11-17 22:27:05 +0530

    Ignore PM2 error when there is no service to stop

--- before-build.sh
+++ before-build.sh
@@ -1,3 +1,7 @@
 pm2 stop drn-api
 
+# PM2 errors out when there is no service to stop. Which causes Github Actions
+# to abort
+#
+# To avoid that we are exiting with 0 status as if there was no error
 exit 0
