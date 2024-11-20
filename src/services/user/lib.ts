3816a6b5f13aa9c38704e06642f41f1364b14d46
Santosh Bandichode <santoshg550@gmail.com>
2024-11-20 23:14:27 +0530

    No more log for empty updates

--- src/services/user/lib.ts
+++ src/services/user/lib.ts
@@ -5,6 +5,15 @@ import Activity, { ActivityData } from './models/activity'
 
 export class UserLib {
     logActivity = async (data: ActivityData, transaction?: Transaction) => {
+        const jsonData = data.data
+        if (jsonData) {
+            if (Object.keys(jsonData).length === 0)
+                return
+
+            if (!jsonData['diff'] || Object.keys(jsonData['diff']).length === 0)
+                return
+        }
+
         return Activity.create(data, { transaction })
     }
 }
