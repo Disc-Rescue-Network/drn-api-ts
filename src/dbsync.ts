41184266a3c9c7bf21146f73420a1376196d3e99
Santosh Bandichode <santoshg550@gmail.com>
2024-11-22 21:40:13 +0530

    Commit script to sync with database

--- src/dbsync.ts
+++ src/dbsync.ts
@@ -1,28 +1,51 @@
 import config from './config'
 import store from './store'
 
-import UserAccount from './services/user/models/account'
 import Brand from './services/brand/models/brand'
 import DiscMold from './services/disc/models/disc'
+import PlasticType from './services/disc/models/plastic-type'
 import Course from './services/course/models/course'
 import Inventory from './services/inventory/models/inventory'
 import SMSLogs from './services/sms/models/sms-logs'
 import PhoneOptIn from './services/sms/models/phone-opt-in'
+import Claim from './services/inventory/models/claim'
+import Pickup from './services/inventory/models/pickup'
+import VerificationOTP from './services/inventory/models/verification-otp'
+import Notification from './services/notification/models/notification'
+import Activity from './services/user/models/activity'
 
 
 ;(async function() {
-    await config.init()
-    await store.init()
+    try {
+        await config.init()
+        await store.init()
 
-    if (process.env.TABLE_SYNC) {
-        await UserAccount.sync({ alter: true })
-        await Brand.sync({ alter: true })
-        await DiscMold.sync({ alter: true })
-        await Course.sync({ alter: true })
-        await Inventory.sync({ alter: true })
-        await SMSLogs.sync({ alter: true })
-        await PhoneOptIn.sync({ alter: true })
+        if (process.env.TABLE_SYNC) {
+            await Course.sync({ alter: true })
+            await Brand.sync({ alter: true })
+            await PlasticType.sync({ alter: true })
+            await DiscMold.sync({ alter: true })
+
+            /*
+             * claimBy column is generated. Syncing along with it would cause
+             * err. Syncing without it would drop it.
+             */
+            await Inventory.sync({ alter: true })
+
+            await SMSLogs.sync({ alter: true })
+            await PhoneOptIn.sync({ alter: true })
+
+            await Claim.sync({ alter: true })
+            await Pickup.sync({ alter: true })
+            await VerificationOTP.sync({ alter: true })
+
+            await Notification.sync({ alter: true })
+
+            await Activity.sync({ alter: true })
+        }
+
+        await store.close()
+    } catch(err) {
+        console.log(err)
     }
-
-    await store.close()
 })()
